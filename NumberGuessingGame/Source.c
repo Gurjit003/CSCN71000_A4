028f240686aa0ffd92de8bfa689f1ac896857446
Gurjit Singh <gurjitsangha03@outlook.com>
2021-12-11 11:34:58 -0500

    Some changes

--- NumberGuessingGame/Source.c
+++ NumberGuessingGame/Source.c
@@ -12,6 +12,12 @@
 #define MAXGUESSES 6  
 
 
+void displayIncorrectGuesses(int guessCount); 
+void displayPreviouslyGuessedNumbers(FILE* fp); 
+void correctNumberMessage(FILE* fp2, int mysteryNumber); 
+void gameOverMessage(FILE* fp2, int mysteryNumber); 
+
+
 int main(void) 
 {
 	FILE* fp = fopen("previousGames.txt", "r");
@@ -22,14 +28,6 @@ int main(void)
 		exit(1);
 	}
 
-	int ch; 
-
-	while ((ch = getc(fp)) != EOF)
-	{
-		putc(ch, stdout);
-	}
-
-
 	FILE* fp2 = fopen("previousGames.txt", "a");
 
 	if (fp == NULL)
@@ -38,19 +36,26 @@ int main(void)
 		exit(1);
 	}
 
+
 	int mysteryNumber;
 	int guess = 0;
-	int guessCount = 0;
+	int guessCount = 0; 
 	int guessTrailCount;
 	
 
-	printf("\nEnter a number between %d and %d (inclusive): ", MINNUMBER, MAXNUMBER);
+	printf("Enter a number between %d and %d (inclusive): ", MINNUMBER, MAXNUMBER);
 	scanf_s("%d", &mysteryNumber); 
 
-	printf("\nNumber of incorrect guesses: %d", guessCount);
+	printf("\n"); 
+	displayIncorrectGuesses(guessCount); 
+
 
 	do {
-		printf("\nEnter a guess: "); 
+		// Read previous successfull and unsuccessfull guesses from file 
+		displayPreviouslyGuessedNumbers(fp); 
+		
+
+		printf("Enter a guess: "); 
 		scanf_s("%d", &guess);
 		
 		guessTrailCount = 0;
@@ -68,18 +73,17 @@ int main(void)
 				{
 					++guessCount; 
 					printf("\nYour guess was too low\n\n"); 
-					printf("Number of incorrect guesses: %d", guessCount); 
+					displayIncorrectGuesses(guessCount); 
 				}
 				else if (guess > mysteryNumber)
 				{
 					++guessCount; 
 					printf("\nYour guess was too high\n\n"); 
-					printf("Number of incorrect guesses: %d", guessCount);
+					displayIncorrectGuesses(guessCount); 
 				}
 				else if (guess == mysteryNumber)
 				{
-					printf("\nYou have correctly guessed the mystery number!\n"); 
-					fprintf(fp2, "%d: Guessed correctly\n", mysteryNumber); 
+					correctNumberMessage(fp2, mysteryNumber); 
 					exit(1); 
 				}
 			}
@@ -87,21 +91,20 @@ int main(void)
 			{ 
 				printf("\nInvalid guess entered, please enter a guess in the correct range (between 1 and 1000 inclusive)\n\n"); 
 				++guessCount; 
-				printf("Number of incorrect guesses: %d", guessCount);
+				displayIncorrectGuesses(guessCount); 
 			}
 		}
 		else
 		{ 
 			printf("\nInvalid guess entered, please enter a number guess\n\n"); 
 			++guessCount; 
-			printf("Number of incorrect guesses: %d", guessCount);
+			displayIncorrectGuesses(guessCount); 
 		}
 	} while (guessCount != MAXGUESSES);
 
 	if (guessCount == MAXGUESSES)
 	{
-		printf("\nYou have now entered 6 guesses. BYE!\n"); 
-		fprintf(fp2, "%d: X\n", mysteryNumber); 
+		gameOverMessage(fp2, mysteryNumber); 
 	}
 
 
@@ -110,4 +113,37 @@ int main(void)
 
 
 	return 0; 
+} 
+
+
+void displayIncorrectGuesses(int guessCount)
+{
+	printf("Number of incorrect guesses: %d", guessCount); 
+} 
+
+
+void displayPreviouslyGuessedNumbers(FILE* fp) 
+{
+	int ch;
+
+	printf("\n"); 
+
+	while ((ch = getc(fp)) != EOF)
+	{
+		putc(ch, stdout);
+	} 
+} 
+
+
+void correctNumberMessage(FILE* fp2, int mysteryNumber)
+{
+	printf("\nYou have correctly guessed the mystery number!\n");
+	fprintf(fp2, "%d: Guessed correctly\n", mysteryNumber);
+} 
+
+
+void gameOverMessage(FILE* fp2, int mysteryNumber)
+{
+	printf("\nYou have now entered 6 guesses. BYE!\n");
+	fprintf(fp2, "%d: X\n", mysteryNumber); 
 } 
